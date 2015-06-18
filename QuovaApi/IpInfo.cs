9c040fbb4a355e83dc187f206eec668db06fc4c9
lazyf1sh <f1sh@pm.me>
2015-06-18 12:47:48 +0300

    initial commit

---
+++ QuovaApi/IpInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace QuovaApi
+{
+    [Serializable()]
+    [XmlRoot("ipinfo", Namespace = "http://data.quova.com/1", IsNullable = false)]
+    public class IpInfo
+    {
+        public string ip_address { get; set; }
+        public string ip_type { get; set; }
+        public ipinfoNetWork Network { get; set; }
+        public ipinfoLocation Location { get; set; }
+    }
+
+    public class ipinfoNetWork
+    {
+        public string organization { get; set; }
+        public string carrier { get; set; }
+        public string asn { get; set; }
+        public string connection_type { get; set; }
+        public string line_speed { get; set; }
+        public string ip_routing_type { get; set; }
+        public ipinfoNetWorkDomain Domain { get; set; }
+    }
+
+    public class ipinfoNetWorkDomain
+    {
+        public string tld { get; set; }
+        public string sld { get; set; }
+    }
+
+    public class ipinfoLocation
+    {
+        public string continent { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public ipinfoLocationCountryData CountryData { get; set; }
+        public string region { get; set; }
+        public ipinfoLocationStateData StateData { get; set; }
+        public string dma { get; set; }
+        public string msa { get; set; }
+        public ipinfoLocationCityData CityData { get; set; }
+    }
+
+    public class ipinfoLocationCountryData
+    {
+        public string country { get; set; }
+        public string country_code { get; set; }
+        public string country_cf { get; set; }
+    }
+    public class ipinfoLocationStateData
+    {
+        public string state { get; set; }
+        public string state_code { get; set; }
+        public string state_cf { get; set; }
+    }
+    public class ipinfoLocationCityData
+    {
+        public string city { get; set; }
+        public string postal_code { get; set; }
+        public string time_zone { get; set; }
+        public string area_code { get; set; }
+        public string city_cf { get; set; }
+    }
+
+}
---
+++ QuovaApi/Quova.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace QuovaApi
+{
+    public class Quova
+    {
+        private string apikey_;
+        private string secret_;
+        public Quova(string apikey, string secret)
+        {
+            this.apikey_ = apikey;
+            this.secret_ = secret;
+        }
+
+        public IpInfo LookUp(string ip)
+        {
+            string sig = MD5GenerateHash(apikey_ + secret_ + (Int32)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds);
+            string url = string.Format("http://api.quova.com/v1/ipinfo/{0}?apikey={1}&sig={2}&format=xml", ip, apikey_, sig);
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                using (StreamReader responseReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string xml_contents = responseReader.ReadToEnd();
+
+                    IpInfo ipInfo = null;
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(IpInfo));
+                    using (XmlReader xmlReader = XmlReader.Create(new StringReader(xml_contents)))
+                    {
+                        ipInfo = (IpInfo)serializer.Deserialize(xmlReader);
+                    }
+
+                    return ipInfo;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private string MD5GenerateHash(string strInput)
+        {
+            MD5 md5Hasher = MD5.Create();
+
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(strInput));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int nIndex = 0; nIndex < data.Length; ++nIndex)
+            {
+                sBuilder.Append(data[nIndex].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
